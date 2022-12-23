9d1fe289a49bd4ca7a70791d736658e257c10020
Pawel1894 <pawepohl@gmail.com>
2022-12-23 14:33:46 +0100

    Errors enum edit

--- src/enums.ts
+++ src/enums.ts
@@ -9,5 +9,5 @@ export enum ERRORS {
   REQUIRED = "This field is required",
   NUMBERS = "This field can't contain numbers",
   EMAIL = "Not a valid email address",
-  INVALID_TEL = "Not a valid tel number",
+  INVALID_TEL = "Not a valid phone number",
 }
